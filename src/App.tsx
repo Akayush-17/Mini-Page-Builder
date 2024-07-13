eceb74104316bb16c3acbd5883ea1028c18f85e1
Akayush-17 <aaayushsinghh@gmail.com>
2024-07-13 18:55:52 +0530

    added instruction to operate in mobile screen

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Sidebar from "./components/Sidebar";
 
 function App() {
   const [touchData, setTouchData] = useState<string | null>(null);
+  const [ClickInstruction, setClickInstruction] = useState(true);
   const storedElements = localStorage.getItem("boardElements");
   const initialBoardElements: {
     id: string;
@@ -22,6 +23,9 @@ function App() {
     localStorage.setItem("boardElements", JSON.stringify(boardElements));
   }, [boardElements]);
 
+  const handleInstruction = () => {
+    setClickInstruction(!ClickInstruction);
+  };
   const handleDrop = (
     elementType: string,
     top: number,
@@ -73,7 +77,10 @@ function App() {
     );
   };
 
-  const handleDragStart = (e: React.DragEvent | React.TouchEvent, id: string) => {
+  const handleDragStart = (
+    e: React.DragEvent | React.TouchEvent,
+    id: string
+  ) => {
     if ("dataTransfer" in e) {
       e.dataTransfer.setData("text", id);
     } else {
@@ -138,8 +145,77 @@ function App() {
           setTouchData={setTouchData}
         />
       </div>
+      {ClickInstruction ? null : <div onClick={handleInstruction}>
+        <svg
+          width="25px"
+          height="25px"
+          viewBox="-0.5 0 25 25"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M12 22.4199C17.5228 22.4199 22 17.9428 22 12.4199C22 6.89707 17.5228 2.41992 12 2.41992C6.47715 2.41992 2 6.89707 2 12.4199C2 17.9428 6.47715 22.4199 12 22.4199Z"
+            stroke="#000000"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M10.5596 8.41992L13.6196 11.29C13.778 11.4326 13.9047 11.6068 13.9914 11.8015C14.0781 11.9962 14.123 12.2068 14.123 12.4199C14.123 12.633 14.0781 12.8439 13.9914 13.0386C13.9047 13.2332 13.778 13.4075 13.6196 13.55L10.5596 16.4199"
+            stroke="#000000"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+      </div>
+      }
+      
+      {ClickInstruction ? (
+        <div>
+          <div
+            className="bg-purple-200 max-w-fit relative
+         h-full p-2"
+          >
+            <div
+              onClick={handleInstruction}
+              className="absolute top-1 right-1 hover:bg-slate-500 rounded-full"
+            >
+              <svg
+                width="20px"
+                height="20px"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  clip-rule="evenodd"
+                  d="M10.9393 12L6.9696 15.9697L8.03026 17.0304L12 13.0607L15.9697 17.0304L17.0304 15.9697L13.0607 12L17.0303 8.03039L15.9696 6.96973L12 10.9393L8.03038 6.96973L6.96972 8.03039L10.9393 12Z"
+                  fill="#080341"
+                />
+              </svg>
+            </div>
+            <h2 className="font-bold text-red-500">**Instruction</h2>
+            <ul className="flex flex-col whitespace-nowrap text-sm ">
+              <li>
+                <span className="font-bold"> Click</span> on element to add to
+                Board
+              </li>
+              <li>
+                <span className="font-bold"> Click</span> on element to open
+                edit modal
+              </li>
+            </ul>
+          </div>
+        </div>
+      ) : null}
+
       <div className="md:w-1/4 w-full md:h-full absolute md:relative bottom-0 bg-[#2D2D2D] pb-2">
-        <Sidebar onDragStart={handleDragStart} onElementCreate={handleElementCreate} />
+        <Sidebar
+          onDragStart={handleDragStart}
+          onElementCreate={handleElementCreate}
+        />
         <div className="md:absolute  bottom-20 md:bg-transparent bg-[#2D2D2D] right-5 gap-4 flex justify-center  flex-col">
           <div className="flex md:flex-wrap gap-4 justify-center items-center w-full">
             <input
